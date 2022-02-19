b706177da126a58a99686070031493a4f0ba51a9
klongrich <longrichk@gmail.com>
2022-02-19 18:57:42 -0500

    hot fix and update for viewing wallet NFTs

---
+++ pages/app/account.tsx
@@ -0,0 +1,167 @@
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { createAlchemyWeb3 } from "@alch/alchemy-web3";
+
+
+const Header = styled.div`
+background-color: #F4A7A7;
+height: 40px;
+width: 100%;
+margin-top: 0px;
+`
+
+const Container = styled.div`
+background-color: #CDA3A6;
+width: 100%;
+margin-top: -40px;
+
+height: 1000px;
+
+h2 {
+    width: 250px;
+    height: 42px;
+    font-family: Roboto;
+    font-size: 48px;
+    line-height: 45px;
+    margin-left: 30px;
+    padding-top: 40px;
+    text-decoration: underline;
+}
+
+
+`
+
+const PictureContainer = styled.div`
+
+margin: 50px;
+
+
+img {
+    margin: 20px;
+    border: 1px solid black;
+    border-radius: 10px;
+}
+
+`
+
+const NoNFTSContainer = styled.div`
+
+    padding: 30px;
+
+    ul {
+        list-style-type: none;
+    }
+
+    li {
+        padding-top: 5px;
+        padding-bottom: 5px;
+    }
+
+`
+
+
+//Fix so that if pulls NFTs once the user connects or reconnects to meta-mask.
+export function Account({ userAddress, web3 }: any) {
+
+    const [userNFTs, setUserNFTs] = useState([{ name: null, image: null }]);
+    const [hasNFTs, setHashNFTs] = useState(true);
+
+    function checkIPFShash(imageURL: any) {
+        var temp = imageURL.substring(0, 4);
+
+        if (temp == "ipfs") {
+            var rawIPFShash = imageURL.substring(7, 53);
+            return ("https://ipfs.io/ipfs/" + rawIPFShash);
+        } else {
+            return (imageURL);
+        }
+    }
+
+    async function getUserNFTs() {
+        const web3 = createAlchemyWeb3(
+            "https://eth-mainnet.alchemyapi.io/v2/UEzIhzfQD4trLHLg2IxfwwukrxfoYk-Q",
+        );
+
+        console.log("Useraddress: " + userAddress);
+
+        const nfts = await web3.alchemy.getNfts({ owner: userAddress })
+
+        console.log(nfts);
+
+        if (nfts.ownedNfts.length <= 0) {
+            console.log("not NFTs on this Account");
+            setHashNFTs(false);
+            return (0);
+        }
+
+        for (let i = 0; i < nfts.ownedNfts.length; i++) {
+            console.log(nfts.ownedNfts[i].contract.address);
+            console.log(parseInt(nfts.ownedNfts[i].id.tokenId, 16));
+
+            let TokenID = parseInt(nfts.ownedNfts[i].id.tokenId, 16);
+
+            const response = await web3.alchemy.getNftMetadata({
+                contractAddress: nfts.ownedNfts[i].contract.address,
+                tokenId: TokenID.toString()
+            });
+
+            //@ts-ignore
+            setUserNFTs(userNFTs => [...userNFTs, response.metadata])
+        }
+
+
+
+    }
+
+
+    useEffect(() => {
+        getUserNFTs();
+        console.log("Hello World");
+    }, [])
+
+    return (
+        <>
+            <Header />
+
+            <Container>
+                <h2> User NFTs </h2>
+
+                {hasNFTs && <>
+                    <PictureContainer>
+                        {userNFTs.map((data) =>
+                            <>
+                                {data.name != null && <>
+                                    <img src={checkIPFShash(data.image)} alt='' height={120} width={120} />
+                                </>}
+
+                            </>
+                        )}
+                    </PictureContainer>
+                </>}
+
+                {!hasNFTs &&
+                    <>
+                        <NoNFTSContainer>
+                            <p> This Account Does not have any NFTs yet! </p>
+
+                            <p> Purchase one today from our partners!</p>
+
+                            <ul>
+                                <a href="https://opensea.io"><li>opensea</li> </a>
+                                <a href="https://rarible.com/"><li>rareabile</li> </a>
+                                <a href="https://zora.co/"><li>Zora </li> </a>
+                                <a href="https://looksrare.org/"><li>Looks Rare </li></a>
+                            </ul>
+
+                            <p> Or Disconnect Web3 wallet and connect to one with NFTs</p>
+                        </NoNFTSContainer>
+                    </>
+                }
+            </Container>
+            <p> Hello World </p>
+        </>
+    )
+
+}
+
+export default Account;
--- pages/app/index.tsx
+++ pages/app/index.tsx
@@ -5,10 +5,13 @@ import Web3 from 'web3';
 import Web3Modal from 'web3modal';
 import WalletConnectProvider from '@walletconnect/web3-provider';
 
-import { User } from "@styled-icons/boxicons-regular/User"
+import { User } from "@styled-icons/boxicons-regular/User";
+import { Backspace } from "@styled-icons/fluentui-system-regular/Backspace";
 
 import Dashboard from "./dashboard";
 import MobileDashboard from "./mobileDashboard";
+
+import Account from "./account";
 
 //Move infuraID to .env file
 const providerOptions = {
@@ -66,6 +69,32 @@ const AccountButton = styled.div`
 
     padding-left: 6px;
     padding-top: 9px;
+
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+
+    :hover {
+        background-color: #FDC8C7;
+        cursor: pointer;
+    }
+
+`
+
+const BackArrow = styled.div`
+    background-color: #F4A7A7;
+
+    height: 35px;
+    width: 44px;
+    
+    float: right;
+
+    border: 1px solid black;
+
+    margin-right: 90px;
+
+    padding-left: 6px;
+
+    padding-top: 2px;
+    padding-bottom: 6px;
 
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
 
@@ -136,10 +165,12 @@ const AccountButtonMobile = styled.div`
 
 export function Dapp(): any {
     //const [provider, setProvider] = useState('');
-    //const [web3, setWeb3] = useState('');
 
-    const [userAddress, setUserAddress] = useState('');
+    const [web3, setWeb3] = useState(new Web3());
     const [isMobile, setIsMobile] = useState(false);
+    const [userAddress, setUserAddress] = useState('');
+
+    const [state, setState] = useState("Collections");
 
     async function loadWeb3() {
         if (typeof window !== "undefined") {
@@ -152,12 +183,10 @@ export function Dapp(): any {
             const provider = await web3Modal.connect();
             const web3 = await new Web3(provider);
 
-            // setProvider(provider);
-            // setWeb3(web3);
-
             if (web3) {
                 const EthAccounts = await web3.eth.getAccounts();
                 setUserAddress(EthAccounts[0]);
+                setWeb3(web3);
             } else {
                 console.log('web3 not found');
             }
@@ -168,8 +197,8 @@ export function Dapp(): any {
         return (address.substring(0, 4) + "...." + address.substring(38, 42));
     }
 
-    useEffect(() => {
-        loadWeb3();
+    useEffect(async () => {
+        await loadWeb3();
         if (typeof window != "undefined") {
             if (window.innerWidth > 999) {
                 setIsMobile(false);
@@ -181,38 +210,70 @@ export function Dapp(): any {
 
     return (
         <>
-            {!isMobile &&
-                <>
-                    <Header>
-                        <AccountButton>
-                            <User width={40} height={40} />
-                        </AccountButton>
 
-                        <ConnectWallet onClick={() => loadWeb3()}>
-                            {userAddress && <> {parseUserAddress(userAddress)} </>}
-                            {!userAddress && <> Connect Wallet </>}
-                        </ConnectWallet>
-                    </Header>
+            { state == "Collections" && <>
+                {!isMobile &&
+                    <>
+                        <Header>
+                            <AccountButton onClick={() => setState("Account")}>
+                                <User width={40} height={40} />
+                            </AccountButton>
 
-                    <Dashboard userAddress={userAddress} />
-                </>
+                            <ConnectWallet onClick={() => loadWeb3()}>
+                                {userAddress && <> {parseUserAddress(userAddress)} </>}
+                                {!userAddress && <> Connect Wallet </>}
+                            </ConnectWallet>
+                        </Header>
+
+                        <Dashboard userAddress={userAddress} />
+                    </>
+                }
+
+                {isMobile &&
+                    <>
+                        <HeaderMobile>
+                            <AccountButtonMobile>
+                                <User width={40} height={40} />
+                            </AccountButtonMobile>
+
+                            <ConnectWalletMobile onClick={() => loadWeb3()}>
+                                {userAddress && <> {parseUserAddress(userAddress)} </>}
+                                {!userAddress && <> Connect Wallet </>}
+                            </ConnectWalletMobile>
+                        </HeaderMobile>
+
+                        <MobileDashboard userAddress={userAddress} />
+                    </>
+                }
+            </>
             }
 
-            {isMobile &&
-                <>
-                    <HeaderMobile>
-                        <AccountButtonMobile>
-                            <User width={40} height={40} />
-                        </AccountButtonMobile>
+            { state == "Account" && <>
+                {!isMobile &&
+                    <>
 
-                        <ConnectWalletMobile onClick={() => loadWeb3()}>
-                            {userAddress && <> {parseUserAddress(userAddress)} </>}
-                            {!userAddress && <> Connect Wallet </>}
-                        </ConnectWalletMobile>
-                    </HeaderMobile>
+                        <Header>
+                            <BackArrow onClick={() => setState("Collections")}>
+                                <Backspace width={40} height={40} />
+                            </BackArrow>
 
-                    <MobileDashboard userAddress={userAddress} />
-                </>
+                            <ConnectWallet onClick={() => loadWeb3()}>
+                                {userAddress && <> {parseUserAddress(userAddress)} </>}
+                                {!userAddress && <> Connect Wallet </>}
+                            </ConnectWallet>
+                        </Header>
+
+                        <Account userAddress={userAddress}
+                            web3={web3} />
+
+                    </>
+                }
+                {isMobile &&
+                    <>
+
+                    </>
+                }
+            </>
             }
         </>
     )
